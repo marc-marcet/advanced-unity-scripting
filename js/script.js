458ac022ee86374c6738592cdb5bc4e61ee906f0
marc-marcet <95296064+marc-marcet@users.noreply.github.com>
2021-12-28 22:35:58 +0100

    Entrega 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+/* DESCRIPTION: CUSTOM JS FILE */
+
+/* NAVIGATION*/
+// COLLAPSE THE NAVBAR BY ADDING THE TOP-NAV-COLLAPSE CLASS
 window.onscroll = function () {
-	scrollFunction();   // nav collapse
+	scrollFunction();
 	scrollFunctionBTT(); // back to top button
 };
 
@@ -18,10 +22,120 @@ function scrollFunction() {
 	}
 }
 
-function scrollFunctionBTT() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        myButton.style.display = "block";
-    } else {
-        myButton.style.display = "none";
+// NAVBAR ON MOBILE
+let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", () => {
+        document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+}
+
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  document.querySelector(".offcanvas-collapse").classList.toggle("open");
+});
+
+// HOVER ON DESKTOP
+function toggleDropdown(e) {
+    const _d = e.target.closest(".dropdown");
+    let _m = document.querySelector(".dropdown-menu", _d);
+
+    setTimeout(
+        function () {
+        const shouldOpen = _d.matches(":hover");
+        _m.classList.toggle("show", shouldOpen);
+        _d.classList.toggle("show", shouldOpen);
+
+        _d.setAttribute("aria-expanded", shouldOpen);
+        },
+        e.type === "mouseleave" ? 300 : 0
+    );
+}
+
+// ON HOVER
+const dropdownCheck = document.querySelector(".dropdown");
+
+if (dropdownCheck !== null) {
+    document
+        .querySelector(".dropdown")
+        .addEventListener("mouseleave", toggleDropdown);
+    document
+        .querySelector(".dropdown")
+        .addEventListener("mouseover", toggleDropdown);
+
+    // ON CLICK
+    document.querySelector(".dropdown").addEventListener("click", (e) => {
+        const _d = e.target.closest(".dropdown");
+        let _m = document.querySelector(".dropdown-menu", _d);
+        if (_d.classList.contains("show")) {
+            _m.classList.remove("show");
+            _d.classList.remove("show");
+        } else {
+            _m.classList.add("show");
+            _d.classList.add("show");
+        }
+    });
+}
+
+
+// Carousel
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-33.34%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains('third')){
+      slides.style.transform = 'translatex(-66.67%)';
+      e.target.classList.add('active');
     }
+  }
+});
+
+
+// Json
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
+
+window.onload = function(){
+  var data;
+  var a = document.querySelector('.inner');
+
+  readTextFile("dades.json", function(text){
+      data = JSON.parse(text);
+      
+      for (var i = 0; i < data.length; i++) {
+        var h = document.createElement('div');
+        h.classList = "card swiper-slide slider";
+        var inner = document.createElement('div');
+        inner.classList = "d-flex center";
+        var name = document.createElement('h6');
+        var img = document.createElement('img');
+        img.classList = "avatar";
+        img.src = data[i]['img'];
+        name.appendChild(document.createTextNode(data[i]['name']));
+        inner.appendChild(img);
+        inner.appendChild(name);
+        h.appendChild(document.createTextNode(data[i]['text']));
+        h.appendChild(inner);
+        a.appendChild(h);
+      }
+  });
 }
